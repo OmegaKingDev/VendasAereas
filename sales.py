dfa0dcbac7c93ed2ab481735f5302035a3617e60
Caio <omegakingdev@gmail.com>
2025-05-30 23:56:39 -0300

    Program Version 1.8

--- sales.py
+++ sales.py
@@ -13,10 +13,12 @@ def Fvoos():
         origem = (input(f"digite a origem do voo N°{i}: "))
         destino = (input(f"\nInsira o destino do voo N°{i}: "))
         Infoescalas = int(input("\nInsira a quantidade de escalas: "))
+        passageirosC = 0
         escala = []           
         vooDados.append(origem)
         vooDados.append(destino)
         vooDados.append(Infoescalas)
+        
         if Infoescalas > 0:
             for b in range (Infoescalas):
                 escala.append(input(f"insira o lugar da escala: "))
@@ -27,6 +29,7 @@ def Fvoos():
         lugares = int(input("\nInsira a quantidade de lugares disponiveis: ")) 
         vooDados.append(preco)
         vooDados.append(lugares)
+        vooDados.append(passageirosC)
         voos[vooCodigo] = vooDados
         print(voos)
         
@@ -98,17 +101,7 @@ def Fvenda():
                 dados.append(telefone)
                 passageiros[cpf] = dados
 
-                qt = int(input("Quantas passagens deseja comprar?: "))
-                if voos[indice][2] == 0:
-                    while qt > voos[indice][4] or qt <= 0:
-                        print("A quantidade de passagens que você está tentando comprar não está disponivel.")
-                        qt = int(input("Quantas passagens deseja comprar?\n "))
-                else:
-                    while qt > voos[indice][5] or qt <= 0:
-                        print("A quantidade de passagens que você está tentando comprar não está disponivel.")
-                        qt = int(input("Quantas passagens deseja comprar?\n "))
-                      
-
+                    
                 print("\n Como deseja pagar?")
                 print("1 - Credito")
                 print("2 - debito")
@@ -123,42 +116,98 @@ def Fvenda():
                 c = input("codigo de segurança: ")
                 if compra == 1:
                     if voos[indice][2] == 0:
-                        a = voos[indice][3] 
-                        voos[indice][3] *= qt
                         parcela = int(input("deseja parcelar em quantas vezes? MAXIMO DE 24x\n"))
                         valorFinal = voos[indice][3]/ parcela
                         print(f"perfeito!! sua viagem de {voos[indice][0]} para {voos[indice][1]} que custará R${valorFinal:.02f}, sendo {parcela}x de R${voos[indice][3]} foi agendada! Aproveite suas férias.")
-                        voos[indice][4] = voos[indice][4] - qt
-                        voos[indice][3] = a
+                        voos[indice][4] = voos[indice][4] - 1
+                        passageiros[cpf].append(indice)
+                        voos[indice][5] += 1
+
                     
                     else:
-                        a = voos[indice][4]
-                        voos[indice][4] *= qt
                         parcela = int(input("deseja parcelar em quantas vezes? MAXIMO DE 24x\n"))
                         valorFinal = voos[indice][4]/ parcela
                         print(f"perfeito!! sua viagem de {voos[indice][0]} para {voos[indice][1]} que custará R${valorFinal:.02f}, sendo {parcela}x de R${voos[indice][4]} foi agendada! Aproveite suas férias.")
-                        voos[indice][5] = voos[indice][5] - qt
-                        voos[indice][4] = a
+                        voos[indice][5] = voos[indice][5] - 1
+                        passageiros[cpf].append(indice)
+                        voos[indice][6] += 1
 
 
                 elif voos[indice][2] == 0:
-                    a = voos[indice][3]
-                    voos[indice][3] *= qt
                     print(f"Parabéns! sua viagem de {voos[indice][0]} para {voos[indice][1]} por R${voos[indice][3]} foi agendada! Aproveite suas férias.")
-                    voos[indice][4] = voos[indice][4] - qt
-                    voos[indice][3] = a
-
+                    voos[indice][4] = voos[indice][4] - 1
+                    passageiros[cpf].append(indice)
+                    voos[indice][5] += 1
                 else:
-                    a = voos[indice][3]
-                    voos[indice][4] *= qt
                     print(f"Parabéns! sua viagem de {voos[indice][0]} para {voos[indice][1]} por R${voos[indice][4]} foi agendada! Aproveite suas férias.")
-                    voos[indice][5] = voos[indice][5] - qt
-                    voos[indice][3] = a
-
+                    voos[indice][5] = voos[indice][5] - 1
+                    passageiros[cpf].append(indice)
+                    voos[indice][6] += 1
     return cpf, nome, telefone
 
 def consultaP():
-    a=0
+    encontrados = False
+    res = "N"
+    while res == "N":
+        if not voos:
+            print("Não há voos disponiveis no momento\n")
+            w = (input("(S) para voltar para o menu principal\n")).upper()  
+            while w != "S":
+                w = (input("Valor invalido! (S) para voltar para o menu principal\n")).upper()
+            return #
+                
+        else:
+            print(f"Código dos voos cadastrados: ")      
+        for i in voos.keys():
+            print(f"- {i}")
+        b = input("Deseja continuar ou voltar para o menu principal? (C para continuar) (V para voltar)\n").upper()
+        while b not in ["C", "V"]:
+            b = input("Escolha invalida! Deseja continuar ou voltar para o menu principal? (C para continuar) (V para voltar)\n").upper()
+        if b == "V":
+            return #
+        else:
+            indice = int(input("escolha o N° do voo para ver os passageiros: "))
+            while indice not in voos.keys():
+                print("Esse codigo não existe!")
+                indice = int(input("escolha o N° do voo para ver as informações: "))
+        while indice not in voos.keys():
+            print("Esse código não existe!")
+            indice = int(input("Escolha o N° do voo para ver os passageiros: "))
+    
+        print(f"\nPassageiros do voo {indice}:")
+
+        for cpf, x in passageiros.items():
+            if indice in x:
+                print(f"- CPF: {cpf} | Nome: {x[0]} | Telefone: {x[1]}")
+                encontrados = True
+
+        if not encontrados:
+            print("Não há passageiros neste voo.")
+            return #
+        else:
+            p = input("Deseja remover um passageiro? (S/N)").upper()
+            while p not in ["S","N"]:
+                p = input("Escolha invalida! Deseja remover um passageiro? (S/N)").upper()
+
+
+        if p == "S":
+            cpf = int(input("Digite o CPF do passageiro que deseja remover: "))
+
+            while cpf not in passageiros:
+                print("Passageiro não encontrado.")
+                cpf = int(input("Digite o CPF do passageiro que deseja remover: "))
+            passageiros[cpf].remove(indice)
+            if voos[indice][2] == 0:
+                voos[indice][4] += 1
+                return #
+            else:
+                voos[indice][5] += 1
+                return #
+
+
+
+
+
 
 
     
